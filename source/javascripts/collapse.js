b98d74e904282ac27bf8071a66b6dca44726d2a5
Nathan Huberty <nathan.huberty@orange.fr>
2018-01-07 16:36:23 +0100

    ajout du toggle dans le html

--- source/javascripts/collapse.js
+++ source/javascripts/collapse.js
@@ -1,4 +1,15 @@
-$('.collapse').on('click',function(e){
-  e.preventDefault();
-  $(this).toggleClass('active');
-});
+// $('.collapse').on('click',function(e){
+//   e.preventDefault();
+//   $(this).toggleClass('active');
+// });
+
+// document.querySelector('.collapse').addEventListener('click', function(e) {
+//   e.preventDefault();
+//   this.classList.toggle('active');
+// });
+
+//   e.preventDefault();
+//   e.classList.toggle("active")
+// });
+// [].map.call(document.querySelectorAll('.collapse'), function(el) {
+//   el.classList.toggle('active');
